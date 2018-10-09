5c8e43a41ba811d0860c15defc988eff21b906e8
Leejuwon(school) <bbit456@naver.com>
2018-10-09 18:40:51 +0900

    drill09-4
    작은 공 큰 공 클래스 설정

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -23,6 +23,13 @@ class Boy:
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
+class SBall:
+    pass
+
+class BBall:
+    pass
+
+
 def handle_events():
     global running
     events = get_events()
